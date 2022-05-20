a5e466d839d6626cc8387e0df6d157a013798004
Junsu Park <junsupark94@gmail.com>
2022-05-20 15:28:38 -0700

    Still working on API

--- client/src/ProductOverview/ProductOverview.jsx
+++ client/src/ProductOverview/ProductOverview.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import StarRating from './StarRating.jsx';
 
+const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
+
 function ProductOverview() {
+
+  axios.request({
+    url: API_URL,
+    method: 'get',
+
+  })
 
   return (
     <div>
