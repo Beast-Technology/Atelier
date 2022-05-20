a00b3c14f8ceb8494a3113681dc4a685ea3ec7f2
Junsu Park <junsupark94@gmail.com>
2022-05-20 15:26:53 -0700

    Trying to fix main - scripts, files, and gitignore. Made skeleton of ProductOverview module, need to pull data to see how it's modeled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import RelatedItemsContainer from './RelatedItems/RelatedItemsContainer.jsx';
-
-
-
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
+import ProductOverview from './ProductOverview/ProductOverview.jsx';
 
 function App() {
   return (
     <div>
-      Hello World
-      <div>Hello World</div>
+      <ProductOverview />
       <RatingsAndReviews />
       <RelatedItemsContainer />
     </div>
---
+++ client/src/ProductOverview/ProductOverview.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StarRating from './StarRating.jsx';
+
+function ProductOverview() {
+
+  return (
+    <div>
+      Product Overview
+      <StarRating />
+    </div>
+  )
+}
+
+export default ProductOverview;
