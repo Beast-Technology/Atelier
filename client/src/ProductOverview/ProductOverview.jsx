ed594b4f5e3c7ebe12c53e9b747fb632a14f452b
Junsu Park <junsupark94@gmail.com>
2022-05-20 16:47:06 -0700

    Implementing hooks

--- client/src/ProductOverview/ProductOverview.jsx
+++ client/src/ProductOverview/ProductOverview.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import axios from 'axios';
 import StarRating from './StarRating.jsx';
 
-const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
+const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews';
+const AUTHORIZATION = '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';
 
 function ProductOverview() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    axios.request({
+      url: API_URL,
+      method: 'get',
+      headers: {
+        Authorization: AUTHORIZATION
+      },
+      params: {
+        product_id: 40344
+      }
+    })
+      .then(response => {
+        console.log(response.data.results);
+        setReviews(response.data.results);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
 
   return (
     <div>
       Product Overview
-      <StarRating />
+      <StarRating reviews={reviews}/>
     </div>
   )
 }
--- client/src/ProductOverview/StarRating.jsx
+++ client/src/ProductOverview/StarRating.jsx
@@ -1,9 +1,23 @@
 import React, {useState, useEffect} from 'react';
 
-function StarRating(props) {
+function StarRating({reviews}) {
+
+  console.log(reviews);
 
   return (
-    <div>Star Rating</div>
+    <div>
+      <h1>Star Rating</h1>
+      {/* <div>{reviews}</div> */}
+      {/* {reviews.map(review => (
+        <div>
+          <h4>{review.review_id}</h4>
+          <p>{review.reviewer_name}</p>
+          <p>{review.rating}</p>
+          <p>{review.body}</p>
+          <p>{review.summary}</p>
+        </div>
+      ))} */}
+    </div>
   )
 }
 
