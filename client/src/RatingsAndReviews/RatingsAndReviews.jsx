bc7ce82b3e49f50ded3d87d136865b3a16d7ed0f
John Ong <itsjohnong@gmail.com>
2022-05-20 17:30:38 -0700

    Connected server side to my client side

--- client/src/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/src/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import ProductBreakdown from './ProductBreakdown.jsx';
 import ReviewsList from './ReviewsList.jsx';
@@ -6,6 +7,23 @@ import WriteReview from './WriteReview.jsx';
 import RatingBreakdown from './RatingBreakdown.jsx';
 
 function RatingsAndReviews() {
+  const [reviews, useReviews] = useState();
+
+  useEffect(() => {
+    axios.get('/reviews', {
+      params: {
+        product_id: 40344,
+        page: 1,
+        count: 10
+      }
+    })
+      .then((res) => {
+        console.log(res.data)
+        useReviews(res.data)
+      })
+      .catch((err) => {console.log(err)})
+  },[])
+
   return (
     <section>
       <h2>Ratings and Reviews</h2>
