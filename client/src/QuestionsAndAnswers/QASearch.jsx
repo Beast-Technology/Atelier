e577dc3cbd838dde6927e115c0e794e3cc6d23db
Xiao Han <outsidenewcastle@gmail.com>
2022-05-23 12:43:38 -0700

    Create QAsearch component

---
+++ client/src/QuestionsAndAnswers/QASearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function QASearch() {
+  return (
+    <form>
+      <input type='text' placeholder='Have a question? Search for answers...'></input>
+    </form>
+  )
+}
--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import QASearch from './QASearch.jsx';
 
 function QuestionsAndAnswers() {
   return (
     <section>
       <h2>QUESTIONS & ANSWERS</h2>
+      <QASearch />
     </section>
   );
 }
