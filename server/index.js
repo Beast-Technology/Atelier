6e169f571f404a2df0321d646e54fe0241908af8
Alex <92830192+EtoKruto@users.noreply.github.com>
2022-05-22 22:39:30 -0700

    Implement Axios get on client and server side for products and products related through the Herokuapp API Implement  useState on client to show the array of objects attained from heroku API Use API request Promise all request for multiple axioms gets from API Build basic design of online style of Related Item cards for carousel in future build Design inline elements to be used in CSS due on consensus at later commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config()
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.static(path.join(__dirname, './../client/dist')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+app.get('/products/', (req, res)=> {
+  let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.query.product_id}`
+  axios.get(url,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: process.env.AUTHORIZATION
+      }
+    })
+    .then((response) => {
+      // console.log('data receieved response: ', response);
+      res.send(JSON.stringify(response.data))
+    })
+  })
+
+  app.get('/products/related', (req, res)=> {
+    let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.query.product_id}/related`
+    // console.log(url);
+    axios.get(url,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: process.env.AUTHORIZATION
+        }
+      })
+      .then((response) => {
+        // console.log('data receieved response: ', response);
+        res.send(JSON.stringify(response.data))
+      })
+    })
+
+
+
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
