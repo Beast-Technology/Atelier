283928040dbbe300ee94e4e24cca12e059bd19a9
Junsu Park <junsupark94@gmail.com>
2022-05-22 18:37:45 -0700

    Working on Style Selector image icons

---
+++ client/src/ProductOverview/ProductInformation/StarRating.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function StarRating({reviews}) {
+
+  const starAvg = reviews.reduce((prev, curr) => {
+    const prevRate = prev.rating ? prev.rating : prev;
+    return prevRate + curr.rating;
+  }, 0)/reviews.length;
+
+  return (
+    <div>
+      <p>Star Average: {starAvg}</p>
+      <a href="">Read All Reviews</a>
+    </div>
+  )
+}
+
+export default StarRating;
