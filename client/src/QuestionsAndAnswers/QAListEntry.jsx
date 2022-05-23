02960d5f386427bcf22ed2190608d56145066128
Xiao Han <outsidenewcastle@gmail.com>
2022-05-23 14:20:42 -0700

    Create QAListEntry component

---
+++ client/src/QuestionsAndAnswers/QAListEntry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function QAListEntry() {
+  return (
+    <li>
+      <p>Q: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>A: Fusce augue eros, laoreet et scelerisque non, pretium sit amet augue. In hac habitasse platea dictumst. Ut dui nunc, iaculis et magna non, feugiat aliquam elit.</p>
+    </li>
+  )
+}
