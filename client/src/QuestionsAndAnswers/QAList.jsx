155424edfa074e61e8e0cc17a96002349dcc8071
Xiao Han <outsidenewcastle@gmail.com>
2022-05-23 14:19:57 -0700

    Create QAList component

---
+++ client/src/QuestionsAndAnswers/QAList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import QAListEntry from './QAListEntry.jsx';
+
+export default function QAList() {
+  return (
+    <ul>
+      Questions & Answers List
+      <QAListEntry />
+    </ul>
+  )
+}
