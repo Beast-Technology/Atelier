d48ac46cdab06ce27f79aa5be4572a9fc4dfd6fd
Xiao Han <outsidenewcastle@gmail.com>
2022-05-23 14:30:33 -0700

    Update QuestionsAndAnswers

--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import QASearch from './QASearch.jsx';
+import QAList from './QAList.jsx';
 
 function QuestionsAndAnswers() {
   return (
     <section>
       <h2>QUESTIONS & ANSWERS</h2>
       <QASearch />
+      <QAList />
     </section>
   );
 }
