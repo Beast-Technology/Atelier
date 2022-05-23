c8039f05d75601e1f4a8ff64489337a8f0ad319f
Xiao Han <outsidenewcastle@gmail.com>
2022-05-23 15:39:10 -0700

    Update QAList with Helpful and Add Answers

--- client/src/QuestionsAndAnswers/QAListEntry.jsx
+++ client/src/QuestionsAndAnswers/QAListEntry.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 export default function QAListEntry() {
   return (
     <li>
-      <p>Q: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div>
+        <span>Q: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+        <span>Helpful? Yes (25)</span>
+        <span>Add Answers</span>
+      </div>
       <p>A: Fusce augue eros, laoreet et scelerisque non, pretium sit amet augue. In hac habitasse platea dictumst. Ut dui nunc, iaculis et magna non, feugiat aliquam elit.</p>
     </li>
   )
