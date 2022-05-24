d5ab7becafaa0ce769801faaea6d22f7aa0d14a8
John Ong <itsjohnong@gmail.com>
2022-05-23 17:50:56 -0700

    deleted comments

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -25,35 +25,3 @@ module.exports = {
       })
   }
 }
-
-// app.get('/reviews', (req, res) => {
-//   axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/', {
-//     headers: {
-//       Authorization: TOKEN,
-//     },
-//     params: {
-//       page: req.query.page,
-//       count: req.query.count,
-//       product_id: req.query.product_id,
-//     },
-//   })
-//     .then((response) => {
-//       res.send(response.data);
-//     })
-//     .catch((err) => { res.status(404).send(err); });
-// });
-
-// app.get('/reviews/meta', (req, res) => {
-//   axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/meta', {
-//     headers: {
-//       Authorization: TOKEN,
-//     },
-//     params: {
-//       product_id: req.query.product_id,
-//     },
-//   })
-//     .then((response) => {
-//       res.send(response.data);
-//     })
-//     .catch((err) => { res.status(404).send(err); })
-// })
--- server/index.js
+++ server/index.js
@@ -24,35 +24,3 @@ app.use(express.static(path.join(__dirname, '/../client/dist')));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// app.get('/reviews', (req, res) => {
-//   axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/', {
-//     headers: {
-//       Authorization: TOKEN,
-//     },
-//     params: {
-//       page: req.query.page,
-//       count: req.query.count,
-//       product_id: req.query.product_id,
-//     },
-//   })
-//     .then((response) => {
-//       res.send(response.data);
-//     })
-//     .catch((err) => { res.status(404).send(err); });
-// });
-
-// app.get('/reviews/meta', (req, res) => {
-//   axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/meta', {
-//     headers: {
-//       Authorization: TOKEN,
-//     },
-//     params: {
-//       product_id: req.query.product_id,
-//     },
-//   })
-//     .then((response) => {
-//       res.send(response.data);
-//     })
-//     .catch((err) => { res.status(404).send(err); })
-// })
