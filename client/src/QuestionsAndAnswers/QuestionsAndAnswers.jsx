b1c377dc12b20487674c87647c4273a4bfb9c435
Xiao Han <outsidenewcastle@gmail.com>
2022-05-23 17:44:56 -0700

    Update QuestionsAndAnswers

--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import QASearch from './QASearch.jsx';
 import QAList from './QAList.jsx';
+import './qa-styles.css';
 
 function QuestionsAndAnswers() {
   return (
     <section>
-      <h2>QUESTIONS & ANSWERS</h2>
+      <h2 className="heading heading-secondary">QUESTIONS & ANSWERS</h2>
       <QASearch />
       <QAList />
     </section>
