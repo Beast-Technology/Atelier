3a272f476b1c946f662cbbfb2b5fe08e13e55296
Xiao Han <outsidenewcastle@gmail.com>
2022-05-24 22:07:58 -0700

    Change data fetching from QAList to QuestionsAndAnswers

--- client/src/QuestionsAndAnswers/QAList.jsx
+++ client/src/QuestionsAndAnswers/QAList.jsx
@@ -1,20 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import QAListEntry from './QAListEntry.jsx';
 
-const axios = require('axios');
-
-export default function QAList() {
-  const [qs, setQs] = useState([]);
-
-  useEffect(() => {
-    axios.get('/qa/questions', { params: { product_id: 40355, count: 5 } })
-      .then(res => {
-        console.log('qa list:', res.data.results);
-        setQs(res.data.results);
-        })
-      .catch(err => { alert(err) })
-  }, [])
-
+export default function QAList({qs}) {
   return (
     <div>
       <ul className="qa-list">
@@ -26,6 +13,5 @@ export default function QAList() {
         <a className="btn btn-primary">ADD A QUESTION +</a>
       </div>
     </div>
-
   )
 }
--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import QASearch from './QASearch.jsx';
 import QAList from './QAList.jsx';
 import './qa-styles.css';
 
+const axios = require('axios');
+
 function QuestionsAndAnswers() {
+  const [qs, setQs] = useState([]);
+  // const [displayQs, setDisplayQs] = useState([]);
+
+  useEffect(() => {
+    axios.get('/qa/questions', { params: { product_id: 40355, count: 5 } })
+      .then(res => {setQs(res.data.results)})
+      .catch(err => { alert(err) })
+  }, [])
+
   return (
     <section>
       <h2 className="heading heading-secondary">QUESTIONS & ANSWERS</h2>
       <QASearch />
-      <QAList />
+      <QAList qs={qs}/>
     </section>
   );
 }
