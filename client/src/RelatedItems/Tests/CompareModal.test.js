7c4289d995512453ab55304853b86c21dade0191
Alex <92830192+EtoKruto@users.noreply.github.com>
2022-05-26 00:02:03 -0700

    finished basic HTML and rendering test

--- client/src/RelatedItems/Tests/CompareModal.test.js
+++ client/src/RelatedItems/Tests/CompareModal.test.js
@@ -4,6 +4,7 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import {act} from 'react-dom/test-utils';
 import {createRoot} from 'react-dom/client';
 import CompareModal from './../CompareModal.jsx';
+import RelatedProductsCard from './../RelatedProductsCard.jsx';
 
 let container = null;
 const product1 = {
@@ -22,16 +23,16 @@ const product2 = {
     {feature: 'Mid-Sole', value: 'ControlSupport Arch Bridge'},
     {feature: 'Stitching', value: 'Double Stitch'}
   ],
-  name: "Heir Force Ones"
+  name: "Morning Joggers"
 }
 const product3 = {
   features: [
-    {feature: 'Sole', value: 'Rubber'},
-    {feature: 'Material', value: 'FullControlSkin'},
+    {feature: 'Sole', value: 'Grey'},
+    {feature: 'Material', value: 'Cotton'},
     {feature: 'Mid-Sole', value: 'ControlSupport Arch Bridge'},
-    {feature: 'Stitching', value: 'Double Stitch'}
+    {feature: 'Stitching', value: 'Single Stitch'}
   ],
-  name: "Heir Force Ones"
+  name: "Chinos"
 }
 beforeEach(() => {
   container = document.createElement("div");
@@ -45,27 +46,75 @@ afterEach(() => {
   container = null;
 });
 
-it("does not render the CompareModal since showModal is false:", () => {
-  act(() => {
-    render(<CompareModal
-      showModal={false}
-      />, container);
-  });
-  expect(container.firstChild).toBe(null);
-});
-
-
-it("renders Compare Modal without crashing", () => {
-  act(() => {
-
-    render(<CompareModal
-      showModal={true}
-      currentProduct={product1}
-      clickedItem={product2}
-      />, container);
-
-      console.log(container);
-      expect(container.firstChild.className).toBe("modalCard");
-
+describe("Basic HTML test", () => {
+  it("DOES NOT render the CompareModal since showModal is false:", () => {
+    act(() => {
+      render(<CompareModal
+        showModal={false}
+        />, container);
+      });
+      expect(container.firstChild).toBe(null);
     });
-  });
+
+
+    it("DOES render the CompareModal since showModal is true", () => {
+      act(() => {
+
+        render(<CompareModal
+          showModal={true}
+          currentProduct={product1}
+          clickedItem={product2}
+          />, container);
+
+          expect(container.firstChild.className).toBe("modalCard");
+
+        });
+      });
+    });
+    describe("Table Render Test", () => {
+      it("Confirm Title and Checkmarks show up on both features when products are the same", () => {
+        act(() => {
+
+          render(<CompareModal
+            showModal={true}
+            currentProduct={product1}
+            clickedItem={product2}
+            />, container);
+
+            let test1 = document.getElementsByTagName("th")[0].textContent;
+            let test2 = document.getElementsByTagName("th")[2].textContent;
+
+            let test3 = document.getElementsByTagName("tr")[1].getElementsByTagName("td")[0].textContent
+            let test4 = document.getElementsByTagName("tr")[1].getElementsByTagName("td")[2].textContent
+
+            expect(test1).toBe("Heir Force Ones");
+            expect(test2).toBe("Morning Joggers");
+
+            expect(test3).toBe("✓");
+            expect(test4).toBe("✓");
+          });
+        });
+
+        it("Confirm Title and Checkmarks show up on both features when products are the same", () => {
+          act(() => {
+
+            render(<CompareModal
+              showModal={true}
+              currentProduct={product1}
+              clickedItem={product3}
+              />, container);
+
+              let test1 = document.getElementsByTagName("th")[0].textContent;
+              let test2 = document.getElementsByTagName("th")[2].textContent;
+
+              let test3 = document.getElementsByTagName("tr")[1].getElementsByTagName("td")[0].textContent
+              let test4 = document.getElementsByTagName("tr")[1].getElementsByTagName("td")[2].textContent
+
+              expect(test1).toBe("Heir Force Ones");
+              expect(test2).toBe("Chinos");
+
+              expect(test3).toBe("✓");
+              expect(test4).toBe("");
+            });
+          });
+      });
