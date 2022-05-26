07769246c3c3f2d315b493b32586d2e0619cc934
Xiao Han <outsidenewcastle@gmail.com>
2022-05-26 00:03:10 -0700

    Complete search functionality

--- client/src/QuestionsAndAnswers/QASearch.jsx
+++ client/src/QuestionsAndAnswers/QASearch.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function QASearch() {
+export default function QASearch({ keyword, setKeyword }) {
+  function handleChange(e) {
+    setKeyword(e.target.value);
+  }
+
   return (
     <form className="qa-form">
-      <input className="qa-search" type='text' placeholder='HAVE A QUESTION? SEARCH FOR ANSWERS...'></input>
+      <input className="qa-search" type="text" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." value={keyword} onChange={handleChange} />
     </form>
-  )
+  );
 }
--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -5,12 +5,26 @@ import './qa-styles.css';
 
 const axios = require('axios');
 
-const chosenId = 40362;
+const chosenId = 40355;
 
 function QuestionsAndAnswers() {
+  const [qaData, setQAData] = useState([]);
   const [qs, setQs] = useState([]);
   const [displayQs, setDisplayQs] = useState([]);
   const [qsLeft, setQsLeft] = useState(0);
+  const [keyword, setKeyword] = useState('');
+
+  useEffect(() => {
+    if (keyword.length >= 3) {
+      const results = qaData.filter((q) => q.question_body.toLowerCase().includes(keyword.toLowerCase()));
+      setQs(results);
+      setDisplayQs(results.slice(0, 2));
+    } else {
+      setQs(qaData);
+      setDisplayQs(qaData.slice(0, 2));
+    }
+  }, [keyword]);
+
 
   function handleLoad() {
     setDisplayQs([...displayQs, ...qs.slice(displayQs.length, displayQs.length + 2)]);
@@ -19,6 +33,7 @@ function QuestionsAndAnswers() {
   useEffect(() => {
     axios.get('/qa/questions', { params: { product_id: chosenId, count: 100 } })
       .then((res) => {
+        setQAData(res.data.results);
         setQs(res.data.results);
         setDisplayQs(res.data.results.slice(0, 2));
       })
@@ -32,7 +47,7 @@ function QuestionsAndAnswers() {
   return (
     <section className="section-qanda">
       <h2 className="heading heading-secondary">QUESTIONS & ANSWERS</h2>
-      <QASearch />
+      <QASearch keyword={keyword} setKeyword={setKeyword} />
       <QAList qs={displayQs} qsLeft={qsLeft} onHandleLoad={handleLoad} />
     </section>
   );
