0a463df4667205f9863805f45fc8cce9a27f730e
Xiao Han <outsidenewcastle@gmail.com>
2022-05-26 00:06:00 -0700

    Set search to prevent default submit

--- client/src/QuestionsAndAnswers/QASearch.jsx
+++ client/src/QuestionsAndAnswers/QASearch.jsx
@@ -5,8 +5,12 @@ export default function QASearch({ keyword, setKeyword }) {
     setKeyword(e.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="qa-form">
+    <form onSubmit={handleSubmit} className="qa-form">
       <input className="qa-search" type="text" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." value={keyword} onChange={handleChange} />
     </form>
   );
