6db2578887c149fa922a989f708c91a959dff777
Junsu Park <junsupark94@gmail.com>
2022-05-27 09:29:03 -0700

    Added ignore linbreak-style to eslintrc and moved product and reviews state to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import RelatedItems from './RelatedItems/RelatedItems.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
 import ProductOverview from './ProductOverview/ProductOverview.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 
 function App() {
+  // Junsu: added this style globally
+  const style = {
+    border: '2px solid red',
+    width: '1200px',
+    margin: '48px auto',
+    padding: '32px',
+  };
+
+  const [reviews, setReviews] = useState([]);
+  const [product, setProduct] = useState({});
+  const productID = 40346;
+
+  useEffect(() => {
+    axios.request({
+      url: '/reviews',
+      method: 'get',
+      params: {
+        product_id: productID,
+        count: 100,
+      },
+    })
+      .then((response) => {
+        // console.log(response.data);
+        setReviews(response.data.results);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.request({
+      url: `/products/${productID}`,
+      method: 'get',
+    })
+      .then((response) => {
+        // console.log(response.data);
+        setProduct(response.data);
+      });
+  }, []);
+
+
+
   return (
-    <div>
-      <ProductOverview />
+    <div style={style}>
+      <ProductOverview
+        reviews={reviews}
+        product={product}
+      />
       <RatingsAndReviews />
       <QuestionsAndAnswers />
       <RelatedItems />
--- client/src/ProductOverview/ProductOverview.jsx
+++ client/src/ProductOverview/ProductOverview.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProductInformation from './ProductInformation/ProductInformation.jsx';
 import ProductDescription from './ProductDescription.jsx';
@@ -7,37 +7,23 @@ import AddToCart from './AddToCart/AddToCart.jsx';
 import StyleSelector from './StyleSelector/StyleSelector.jsx';
 import ImageGallery from './ImageGallery/ImageGallery.jsx';
 
-function ProductOverview() {
-  const [reviews, setReviews] = useState([]);
-  const [product, setProduct] = useState({});
+function ProductOverview({ reviews, product }) {
+
   const [style, setStyle] = useState({photos: [], skus: {0: {quantity: 0, size: ''}}});
   const [styles, setStyles] = useState([]);
+
   const productID = 40346;
 
-  useEffect(() => {
-    axios.request({
-      url: '/reviews',
-      method: 'get',
-      params: {
-        product_id: productID,
-        count: 100,
-      }
-    })
-      .then(response => {
-        // console.log(response.data);
-        setReviews(response.data.results);
-      })
-  }, []);
 
   useEffect(() => {
     axios.request({
       url: `/products/${productID}`,
       method: 'get',
     })
-      .then(response => {
+      .then((response) => {
         // console.log(response.data);
         setProduct(response.data);
-      })
+      });
   }, []);
 
   useEffect(() => {
@@ -45,28 +31,17 @@ function ProductOverview() {
       url: `/products/${productID}/styles`,
       method: 'get',
     })
-      .then(response => {
+      .then((response) => {
         // console.log(response.data.results[0]);
-        setStyle(response.data.results.find(style => style['default?']));
+        setStyle(response.data.results.find((styleId) => styleId['default?']));
         setStyles(response.data.results);
-      })
-  }, [])
-
-  const containers = {
-    display: 'flex',
-  }
-
-  const section = {
-    border: '2px solid red',
-    width: '1200px',
-    margin: '48px auto',
-    padding: '32px'
-  }
+      });
+  }, []);
 
   return (
-    <div style={section}>
-      <div style={containers}>
-        <ImageGallery photos={style.photos}/>
+    <div id="product-overview" style={{ border: '2px green solid' }}>
+      <div style={{ display: 'flex' }}>
+        <ImageGallery photos={style.photos} />
         <div>
           <ProductInformation
             style={style}
@@ -79,10 +54,10 @@ function ProductOverview() {
             styles={styles}
             setStyle={setStyle}
           />
-          <AddToCart skus={style.skus}/>
+          <AddToCart skus={style.skus} />
         </div>
       </div>
-      <ProductDescription product={product}/>
+      <ProductDescription product={product} />
     </div>
 
   )
