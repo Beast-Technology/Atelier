2e880d03e640c7fc55844df476f8a3c318938fa9
etokruto <akrut26@gmail.com>
2022-06-03 09:56:56 -0700

    work so far

--- server/index.js
+++ server/index.js
@@ -5,19 +5,37 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
+// app.get('*.js', (req, res, next) => {
+//   req.url += '.gz';
+//   res.set('Content-Encoding', 'gzip');
+//   next();
+// });
+
 // Logging and parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
 // Router
 const router = require('./routes.js');
+
 
 // Set up our routes
 app.use(router);
 
+app.get('*.js', (req, res, next) => {
+  if (req.header('Accept-Encoding').includes('br')) {
+    req.url += '.br';
+    console.log(req.header('Accept-Encoding'));
+    res.set('Content-Encoding', 'br');
+    res.set('Content-Type', 'application/javascript; charset=UTF-8');
+  }
+  next();
+});
+
 // Serve the client files
 app.use(express.static(path.join(__dirname, '/../client/dist')));
-
 
 // Set up what we are listening on
 app.listen(port, () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
+const webpack = require('webpack');
 
 const distPath = path.resolve(path.join(__dirname, '/client'), 'dist');
 
@@ -19,11 +22,24 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
-    ]
-  }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin(
+      {
+        'process.env':
+      { NODE_ENV: JSON.stringify('production') },
+      },
+    ),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: '[path].gz[query]', algorithm: 'gzip', test: /\.js$|\.css$|\.html$/, threshold: 10240, minRatio: 0.8,
+    })],
 };
