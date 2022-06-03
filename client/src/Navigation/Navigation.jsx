f4bc1a3fd0c7d52009071644e629696be0051e04
John Ong <itsjohnong@gmail.com>
2022-06-03 16:26:51 -0700

    started on navbar

---
+++ client/src/Navigation/Navigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Navigation-styles.css';
+
+export default function Navbar() {
+  return (
+    <div className="nav-section">
+      <nav className="navigation">
+        <a href="/" className="brand-name">
+          Beast
+        </a>
+        <div className="navigation-menu">
+          <ul>
+            <li>
+              <a href="reviews">Reviews</a>
+            </li>
+            <li>
+              <a href="questions">Questions</a>
+            </li>
+            <li>
+              <a href="related">Related</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ion-icon name="person-circle-outline" style={{width: "30px", height: "30px"}}></ion-icon>
+        </div>
+      </nav>
+    </div>
+  );
+}
